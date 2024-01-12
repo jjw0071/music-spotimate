af5dc13c55b587523c87b070119aff84e5aec8af
MINSEON\minseon <duiph558291@gmail.com>
2024-01-12 22:59:15 +0900

    add file

---
+++ back/src/main/java/kodong/spotimate/SpotimateApplication.java
@@ -0,0 +1,13 @@
+package kodong.spotimate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotimateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotimateApplication.class, args);
+	}
+
+}
